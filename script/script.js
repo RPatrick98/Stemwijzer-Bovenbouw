7488878ca2d3e7cc57a2fe4b8778d497a3713188
RPatrick98 <99049638@mydavinci.nl>
2022-03-07 20:49:45 +0100

    1

---
+++ script/script.js
@@ -0,0 +1,21 @@
+parties.map(p => p.match = 0)
+
+// var main = document.getElementById("main");
+
+// const middle = document.createElement("div");
+
+// const heading = document.createElement("h1");
+// const para = document.createElement("p");
+// const startButton = document.createElement("button");
+
+// heading.innerText = "StemWijzer Tweede kamer 2017";
+// para.innerText = "Test je politieke voorkeur aan de hand van 30 stellingen";
+// startButton.innerText = "Start";
+
+// middle.className = "middle-div";
+
+
+// main.appendChild(middle);
+// middle.appendChild(heading);
+// middle.appendChild(para);
+// middle.appendChild(startButton);
