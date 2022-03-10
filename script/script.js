d9721a226d053b670822928c47c508d78a12c901
RPatryk98 <99049638@mydavinci.nl>
2022-03-10 17:51:17 +0100

    5

--- script/script.js
+++ script/script.js
@@ -1,17 +1,6 @@
-//parties.map(p => p.match = 0)
 
-let startDiv = document.getElementById("main");
-let questionDiv = document.getElementById("qDiv");
 
-let heading = document.getElementById("heading");
-let para = document.getElementById("para");
 
-function startNext() {
-    startDiv.style.display = "none";
-    questionDiv.style.display = "block";
-}
-
-console.log(subjects)
 
 
 
